3ecaf4b4a51bca7b94e5d404ce979cb38ce79f8a
Monicah Ajeso <96474976+Ajeso@users.noreply.github.com>
2022-03-10 14:40:11 +0200

    index.html

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
+const navbar = document.querySelector("#navbar");
+const lastName = navbar.querySelector(".logo span");
+
 window.addEventListener('scroll', function(){
 
   
   if(window.pageYOffset>50) {
     navbar.classList.add("sticky-red");
+    lastName.classList.add("text-white");
   } else {
     navbar.classList.remove("sticky-red");
+    lastName.classList.remove("text-white");
   }
 });
 
-
-window.addEventListener('scroll', function(){
-
-  
-  if(window.pageYOffset>50) {
-    navbar.span.add("span");
-  } else {
-    navbar.span.remove("span");
-  }
-});
